62c7892556932348f88fd6e7b1b5c89a27296e5b
averywolf <avery.a.wolf@gmail.com>
2021-11-05 16:44:37 -0400

    quickly experimented with laser raycast

---
+++ Booster Blade/Assets/Code/Laser.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    private LineRenderer lineRenderer;
+    public Transform laserHit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.useWorldSpace = true; // important I think?
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.up);
+        Debug.DrawLine(transform.position, hit.point);
+        laserHit.position = hit.point;
+        lineRenderer.SetPosition(0, transform.position);
+        lineRenderer.SetPosition(1, laserHit.position);
+
+    }
+}
