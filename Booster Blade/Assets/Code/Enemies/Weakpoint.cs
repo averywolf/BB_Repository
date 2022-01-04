7bcb8e5f26a64a027ec603f6636a17cc02210388
averywolf <avery.a.wolf@gmail.com>
2022-01-04 15:15:52 -0500

    first update in a while
    fixed sorting layer issue with button, not much else.

---
+++ Booster Blade/Assets/Code/Enemies/Weakpoint.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weakpoint : MonoBehaviour
+{
+    // Updated version of EnemyHitbox
+    // EnemyBase to replace EnemyHealth?
+
+    // OnWeakPointHit
+    // grab from static class or something what effect to use?
+
+    public Weakness weakness = Weakness.slashweak; //most enemies die from Slash
+
+    public float reticuleSize;
+
+    public bool weakExposed =true;
+    public enum Weakness
+    {
+        boostweak,
+        slashweak,
+    }
+
+    public void ShowWeakpoint(bool showIt)
+    {
+        
+    }
+}
