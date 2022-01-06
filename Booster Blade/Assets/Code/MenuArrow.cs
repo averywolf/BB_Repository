01d631e43c4ad6499e28b4aa3a0a78c794db9677
averywolf <avery.a.wolf@gmail.com>
2022-01-06 17:46:18 -0500

    improved selection arrow offset

--- Booster Blade/Assets/Code/MenuArrow.cs
+++ Booster Blade/Assets/Code/MenuArrow.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class MenuArrow : MonoBehaviour
 {
+    public float xOffset = 0;
     public void PlaceArrow(RectTransform t)
     {
         Vector2 pos = t.position;
-        pos.x -= t.rect.width / 2f;
+        pos.x -= (t.rect.width / 2f ) + xOffset;
         transform.position = pos;
     }
 }
