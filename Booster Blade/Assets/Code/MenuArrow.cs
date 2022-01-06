3eb1096ab6fea10b00502a1e579b5df555eade3f
averywolf <avery.a.wolf@gmail.com>
2022-01-06 17:33:59 -0500

    made progress on cursors, trying in vain to get gitignore to work

---
+++ Booster Blade/Assets/Code/MenuArrow.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuArrow : MonoBehaviour
+{
+    public void PlaceArrow(RectTransform t)
+    {
+        Vector2 pos = t.position;
+        pos.x -= t.rect.width / 2f;
+        transform.position = pos;
+    }
+}
